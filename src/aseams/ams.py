69e4a6e0f2fa62d5c765b72251b23a6a6d95242d
Thomas Pigeon <thomas.pigeon@ifpen.fr>
2026-02-19 14:25:46 +0100

    little change in numpy encoder

--- src/aseams/ams.py
+++ src/aseams/ams.py
@@ -7,13 +7,14 @@ from ase.io.formats import UnknownFileTypeError
 from ase.parallel import parprint, paropen, world, barrier, broadcast
 from ase.constraints import FixCom
 
+
 class NumpyEncoder(json.JSONEncoder):
     """Special json encoder for numpy types"""
 
     def default(self, obj):
         if isinstance(obj, (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64)):
             return int(obj)
-        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+        elif isinstance(obj, (np.float16, np.float32, np.float64)):
             return float(obj)
         elif isinstance(obj, (np.ndarray,)):
             return obj.tolist()
